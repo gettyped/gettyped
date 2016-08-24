e769c4235659d323032f99d2dd772af44d3e764c
Jeremy Hughes <jedahu@gmail.com>
2016-08-24 16:14:43 +1200

    shell.nix: add nodejs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
 name = "env";
-buildInputs = [caddy emacs gist git sbt];
+buildInputs = [caddy emacs gist git nodejs sbt];
+shellHook = ''
+  export PATH="$PATH:./node_modules/.bin"
+  npm install
+'';
 }
